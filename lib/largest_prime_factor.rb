98f3e213aad2667e2901aa0cb278093118a30fc0
Yechiel Kalmenson <chilikkal@gmail.com>
2016-05-04 07:11:13 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,11 @@
 # Enter your procedural solution here!
+require 'prime'
+def prime_factors(n)
+  factors = Array.new
+  (2...(n/2)).each {|int| factors << int if (int.prime?) && (n % int == 0)}
+  factors
+end
+def largest_prime_factor(n)
+  factors = prime_factors(n)
+  factors[-1]
+end
